bc97b914e147f901fdd8ddc840a415ac428e6e25
Marek Sadło <sadlo.marek.pg2@gmail.com>
2024-11-04 11:49:59 +0100

    czy dziala

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,25 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) return 0;  
+
+    Stocznia stocznia;
+    unsigned int przetransportowane = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    while (przetransportowane < towar) {
+        Statek* statek = stocznia(); 
+
+        
+        przetransportowane += statek->transportuj();
+
+       
+        if (dynamic_cast<Zaglowiec*>(statek) != nullptr) {
+            ++liczbaZaglowcow;
+        }
+
+        delete statek; 
+    }
+
+    return liczbaZaglowcow;
 }
